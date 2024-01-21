f0305a58f3a98b18d18db19d7529f28b44935497
Roy Theunissen <roy.theunissen@live.nl>
2024-01-21 17:41:00 +0100

    Exposed more graph settings via the inspector.

--- Scripts/Runtime/UI/GraphComponent.cs
+++ Scripts/Runtime/UI/GraphComponent.cs
@@ -19,10 +19,19 @@ namespace RoyTheunissen.Graphing
 
             [SerializeField] private Color color = Color.green;
             public Color Color => color;
+            
+            [SerializeField] private GraphLine.Modes mode = GraphLine.Modes.ContinuousLine;
+            public GraphLine.Modes Mode => mode;
+            
+            [SerializeField] private float threshold;
+            public float Threshold => threshold;
         }
         
         [Header("Settings")]
         [SerializeField] private LineSettings[] lineSettings = new LineSettings[1];
+
+        [SerializeField] private float rangeMin;
+        [SerializeField] private float rangeMax = 1.0f;
         
         [Header("Dependencies")]
         [SerializeField] private GraphUI graphUI;
@@ -49,7 +58,21 @@ namespace RoyTheunissen.Graphing
                 return;
             
             didCacheGraph = true;
-            cachedGraph = new Graph(lineSettings[0].Name, lineSettings[0].Color);
+            
+            // Create the graph / the initial line.
+            cachedGraph = new Graph(lineSettings[0].Name, lineSettings[0].Color, null, lineSettings[0].Mode);
+            cachedGraph.SetRange(rangeMin, rangeMax);
+            if (lineSettings[0].Mode == GraphLine.Modes.Threshold)
+                cachedGraph.GetLine(0).SetThreshold(lineSettings[0].Threshold);
+            
+            // Create any additional lines.
+            for (int i = 1; i < lineSettings.Length; i++)
+            {
+                cachedGraph.AddLine(lineSettings[i].Name, lineSettings[i].Color, lineSettings[i].Mode);
+                
+                if (lineSettings[i].Mode == GraphLine.Modes.Threshold)
+                    cachedGraph.GetLine(i).SetThreshold(lineSettings[i].Threshold);
+            }
         }
 
         private void OnValidate()
@@ -57,6 +80,12 @@ namespace RoyTheunissen.Graphing
             // There should always be at least one line.
             if (lineSettings.Length < 1)
                 lineSettings = new LineSettings[1];
+
+            // Made sure the minimum range is always smaller than the maximum range.
+            float range1 = rangeMin;
+            float range2 = rangeMax;
+            rangeMin = Mathf.Min(range1, range2);
+            rangeMax = Mathf.Max(range1, range2);
         }
     }
 }
